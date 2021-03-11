942967a40c1b6379ba3212a621a501a20800de3a
Lloy94 <mcdos14@narod.ru>
2021-03-12 00:52:43 +0500

    s

--- Exchange/Program.cs
+++ Exchange/Program.cs
@@ -4,9 +4,27 @@ namespace Exchange
 {
     class Program
     {
+        //Тимофеев
+
+        //Написать программу обмена значениями двух переменных:
+        //а) с использованием третьей переменной;
+        //б) *без использования третьей переменной
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите a ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите b ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            // double t = a;
+            //a = b;
+            // b = t;
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine($"{a} {b}");
         }
     }
 }
