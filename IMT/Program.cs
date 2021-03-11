4164953b7679965e28245d7f8f237e83aba6d94d
Lloy94 <mcdos14@narod.ru>
2021-03-12 00:47:04 +0500

    Добавьте файлы проекта.

---
+++ IMT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IMT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
