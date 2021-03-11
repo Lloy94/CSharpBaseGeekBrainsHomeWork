408a8a167d8c307342c967a53e63d72dc3b7b692
Lloy94 <mcdos14@narod.ru>
2021-03-12 00:52:43 +0500

    s

--- Personal/Program.cs
+++ Personal/Program.cs
@@ -4,9 +4,36 @@ namespace Personal
 {
     class Program
     {
+
+        //Тимофеев
+
+
+        //    а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+        //    б) *Сделать задание, только вывод организовать в центре экрана.
+        //    в) **Сделать задание б с использованием собственных методов(например, Print(string ms, int x, int y)
+
+        static void Print(string msg, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(msg);
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(100, 30);
+            Console.WriteLine("Ваше имя ? ");
+            var a = Console.ReadLine();
+            Console.WriteLine("Ваша фамилия ? ");
+            var b = Console.ReadLine();
+            Console.WriteLine("Ваш город проживания ? ");
+            var c = Console.ReadLine();
+            //var x = 40;
+            //var y = 15;
+            // Console.SetCursorPosition(x, y);
+            // Console.WriteLine($"{a} {b} {c}");
+            Print($"{a} {b} {c}", 40, 15);
+
         }
     }
 }
