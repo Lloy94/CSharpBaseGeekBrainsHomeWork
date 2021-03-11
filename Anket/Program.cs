1630a046e1d091ddd26271d12b21cce2aa9f0b36
Lloy94 <mcdos14@narod.ru>
2021-03-12 00:52:43 +0500

    s

--- Anket/Program.cs
+++ Anket/Program.cs
@@ -4,9 +4,34 @@ namespace Anket
 {
     class Program
     {
+        //Тимофеев
+        //Написать программу «Анкета». Последовательно задаются вопросы(имя, фамилия, возраст, рост, вес). В результате вся информация выводится в одну строчку:
+        //а) используя склеивание;
+        //б) используя форматированный вывод;
+        //в) используя вывод со знаком $
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ваше имя ? ");
+            var a = Console.ReadLine();
+            Console.WriteLine("Ваша фамилия ? ");
+            var b = Console.ReadLine();
+            Console.WriteLine("Ваш возраст ? ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ваш  рост в метрах ? ");
+            double d = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ваш вес в килограммах ? ");
+            double e = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(a + " " + b + " " + c + " лет " + d + " в метрах  " + e + " килограмм");
+            Console.WriteLine("{0} {1} {2} лет {3:F2} в метрах {4:F2} килограмм", a, b, c, d, e);
+            Console.WriteLine($"{a} {b} {c} лет {d:F2} в метрах {e:F2} килограмм");
+
+
         }
     }
 }
+
+
--- Methods/Program.cs
+++ Methods/Program.cs
@@ -4,9 +4,34 @@ namespace Methods
 {
     class Program
     {
-        static void Main(string[] args)
+        //Тимофеев
+        //*  Создать класс с методами, которые могут пригодиться в вашей учебе(Print, Pause)
+
+        static void Print(string msg, int x, int y)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(msg);
         }
+
+        private static double RMath(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+
+        private static double Imt()
+        {
+            Console.WriteLine("Введите вес ");
+            double m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите рост ");
+            double h = Convert.ToDouble(Console.ReadLine());
+            double i = m / (h * h);
+            return i;
+        }
+
+        static void Pause()
+        {
+            Console.ReadKey();
+        }
+
     }
 }
