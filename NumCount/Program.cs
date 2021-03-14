f4a5c28131e803b1514858c678eb984060b1094f
Lloy94 <mcdos14@narod.ru>
2021-03-15 01:32:19 +0500

    Revert "Revert "ы""
    This reverts commit a757957c52435de2d14fd89c41fcaed35cd17b28.

---
+++ NumCount/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NumCount
+{
+    //Тимофеев
+    //Написать метод подсчета количества цифр числа.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите a ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            while (a>0) { a = a / 10; s++; };
+            Console.WriteLine($"{s}");
+
+        }
+    }
+}
---
+++ OddSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OddSum
+{
+    class Program
+    {
+
+        //Тимофеев
+        //С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+
+
+
+        static void Main(string[] args)
+        {
+            int b = 0;
+            int x = 0;
+            do
+            {
+                Console.WriteLine("введите число ");
+                b = int.Parse(Console.ReadLine());
+                if (b % 2 == 0) x = x + 0;
+                else x = x + b;
+            }
+            while (b != 0);
+            Console.WriteLine($"{x}");
+           
+         
+        }
+           
+    }
+}
