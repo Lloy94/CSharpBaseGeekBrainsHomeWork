44c6aaefc2df878c562bebc56a60a7665b70ac82
Lloy94 <mcdos14@narod.ru>
2021-03-25 03:26:08 +0500

    ыы

--- LogPass/Program.cs
+++ LogPass/Program.cs
@@ -6,6 +6,9 @@ namespace LogPass
 
     class Program
     {
+
+        //Тимофеев
+        //Решить задачу с логинами из урока 2, только логины и пароли считать из файла в массив. Создайте структуру Account, содержащую Login и Password.
         struct Account
         {
             public string Log;
